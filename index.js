ee17427d8a0207327079f10bd86f1a9f93ab9008
PAULA SOFIA ALBI <albi.paula@gmail.com>
2023-01-17 14:50:54 +0800

    inicio

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//este es el mensaje de bienvenida
+console.log ("esta es la puerta e entrada al proyecto")
+
+/*esta es la parte intermedia de nuestro proyecto
+
+este va a ser el mensaje q se usa para despedir*/
+
+console.log("adios")
